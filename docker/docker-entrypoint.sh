ff8e2672b202a98303cc9138729648abf7b22ff1
Max <max@maxf.net>
2024-04-05 13:48:12 -0400

    Added build scripts

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,6 +3,7 @@
 if [ -z "$1" ] || [ ! -x "cmd/$1" ]; then
 	echo "One of the following arguments is required:"
 	ls -1 cmd/
+	echo "Pass --help as a second parameter for more details"
 	exit 1
 fi
 
